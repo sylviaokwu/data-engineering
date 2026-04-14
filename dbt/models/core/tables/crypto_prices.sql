3a65aaf711bc72774b0f2fedc0d05c5d37c34997
Ogechukwu Sylvia Okwu <sylviaokwu@gmail.com>
2026-04-14 17:51:26 +0000

    added 30 days moving average

--- dbt/models/core/tables/crypto_prices.sql
+++ dbt/models/core/tables/crypto_prices.sql
@@ -5,7 +5,7 @@
 }}
 
 SELECT
-    a.date, ROUND(close,4) as close_price,volume, 
+    a.date, ROUND(close,4) as close_price,volume, ma_30 as moving_average_30d,
     SPLIT(a.ticker, '-')[OFFSET(0)] AS ticker_short,b.sub_asset_class,
     FORMAT_DATE('%A', date) AS day_of_week_name
 FROM  {{ source('public', 'asset_prices') }} a
